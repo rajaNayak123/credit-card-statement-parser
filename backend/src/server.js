dc3978648f0aa55fe3c448e235219def66ab9729
rajaNayak123 <nayakraja151@gmail.com>
2026-02-02 14:00:10 +0530

    fix the import statement of db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import {connectDB} from './config/db.js';
 import statementRoutes from './routes/statementRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
