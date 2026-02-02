7a5c24a22b12404f10d2a51bc0be2e298c11af8f
rajaNayak123 <nayakraja151@gmail.com>
2026-02-02 13:27:48 +0530

    Add the routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import statementRoutes from './routes/statementRoutes.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config()
 
@@ -14,6 +16,11 @@ connectDB()
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/statements', statementRoutes);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000
 
